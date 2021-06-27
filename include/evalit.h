10103572b63a1189a3d52a97dd26bdefc4689779
Bowen Fu <missing>
2021-06-27 09:49:02 +0800

    unify double and constant.

--- include/evalit.h
+++ include/evalit.h
@@ -21,6 +21,14 @@ namespace evalit
             std::shared_ptr<Expr> value;
         };
 
+        struct Constant : std::array<double, 1>
+        {
+        };
+
+        constexpr double pi_ = 3.1415926;
+
+        inline const auto pi = std::make_shared<Expr>(Constant{{pi_}});
+
         struct Sum : std::array<std::shared_ptr<Expr>, 2>
         {
         };
@@ -33,21 +41,38 @@ namespace evalit
         {
         };
 
-        using ExprVariant = std::variant<int32_t, double, Symbol, Sum, Product, Power>;
+        struct Log : std::array<std::shared_ptr<Expr>, 2>
+        {
+        };
+
+        struct Sin : std::array<std::shared_ptr<Expr>, 1>
+        {
+        };
+
+        struct Arctan : std::array<std::shared_ptr<Expr>, 1>
+        {
+        };
+
+        using ExprVariant = std::variant<int32_t, Symbol, Constant, Sum, Product, Power, Log, Sin, Arctan>;
 
         struct Expr : ExprVariant
         {
             using variant::variant;
         };
 
-        inline std::shared_ptr<Expr> value(int32_t v)
+        inline std::shared_ptr<Expr> constant(int32_t v)
         {
             return std::make_shared<Expr>(v);
         }
 
-        inline std::shared_ptr<Expr> value(double v)
+        inline std::shared_ptr<Expr> constant(double v)
         {
-            return std::make_shared<Expr>(v);
+            return std::make_shared<Expr>(Constant{{v}});
+        }
+
+        inline std::shared_ptr<Expr> sin(std::shared_ptr<Expr> const &expr)
+        {
+            return std::make_shared<Expr>(Sin{{expr}});
         }
 
         inline std::shared_ptr<Expr> symbol(std::string const &name)
@@ -72,12 +97,12 @@ namespace evalit
 
         inline std::shared_ptr<Expr> operator-(std::shared_ptr<Expr> const &lhs, std::shared_ptr<Expr> const &rhs)
         {
-            return lhs + value(-1) * rhs;
+            return lhs + constant(-1) * rhs;
         }
 
         inline std::shared_ptr<Expr> operator/(std::shared_ptr<Expr> const &lhs, std::shared_ptr<Expr> const &rhs)
         {
-            return lhs * (rhs ^ value(-1));
+            return lhs * (rhs ^ constant(-1));
         }
 
         inline bool operator==(Expr const &l, Expr const &r)
@@ -97,20 +122,21 @@ namespace evalit
             return match(*ex)(
                 // clang-format off
                 pattern | as<int>(i)                           = expr(i),
-                pattern | as<double>(d)                        = expr(d),
                 pattern | asSymbolDs(e)                        = [&]{ return eval(*e); },
+                pattern | as<Constant>(ds(d))                  = expr(d),
                 pattern | as<Sum>(ds(l, r))                    = [&]{ return eval(*l) + eval(*r); },
                 // Optimize multiplication by 0.
                 pattern | as<Product>(ds(some(as<int>(0)), _)) = expr(0),
                 pattern | as<Product>(ds(_, some(as<int>(0)))) = expr(0),
                 pattern | as<Product>(ds(l, r))                = [&]{ return eval(*l) * eval(*r); },
-                pattern | as<Power>(ds(l, r))                  = [&]{ return std::pow(eval(*l), eval(*r)); }
+                pattern | as<Power>(ds(l, r))                  = [&]{ return std::pow(eval(*l), eval(*r)); },
+                pattern | as<Sin>(ds(e))                       = [&]{ return std::sin(eval(*e)); }
                 // clang-format on
             );
         }
 
     } // namespace impl
-    using impl::value;
+    using impl::constant;
     using impl::symbol;
     using impl::operator+;
     using impl::operator-;
@@ -118,6 +144,8 @@ namespace evalit
     using impl::operator/;
     using impl::operator^;
     using impl::eval;
+    using impl::pi;
+    using impl::sin;
 } // namespace evalit
 
 #endif // EVALIT_H
--- test/evalit/expr.cpp
+++ test/evalit/expr.cpp
@@ -2,55 +2,62 @@
 #include "gtest/gtest.h"
 using namespace evalit;
 
-TEST(Expr, value)
+TEST(Expr, constant1)
 {
-    auto x = value(5);
+    auto x = constant(5);
     EXPECT_EQ(eval(x), 5);
+}
+
+TEST(Expr, constant2)
+{
+    auto expr = sin(pi);
+    EXPECT_NEAR(eval(expr), 0, 1e-5);
+    EXPECT_NEAR(eval(sin(pi / constant(2))), 1, 1e-5);
 }
 
 TEST(Expr, product)
 {
-    auto x = value(5);
-    auto y = value(5);
+    auto x = constant(5);
+    auto y = constant(5);
     EXPECT_EQ(eval(x * y), 25);
 }
 
 TEST(Expr, quotient)
 {
-    auto x = value(5);
-    auto y = value(5);
+    auto x = constant(5);
+    auto y = constant(5);
     EXPECT_EQ(eval(x / y), 1);
 }
 
 TEST(Expr, sum)
 {
-    auto x = value(5);
-    auto y = value(5);
+    auto x = constant(5);
+    auto y = constant(5);
     EXPECT_EQ(eval(x + y), 10);
 }
 
 TEST(Expr, difference)
 {
-    auto x = value(5);
-    auto y = value(5);
+    auto x = constant(5);
+    auto y = constant(5);
     EXPECT_EQ(eval(x - y), 0);
 }
 
 TEST(Expr, compound)
 {
-    auto x = value(5);
-    auto y = value(2);
+    auto x = constant(5);
+    auto y = constant(2);
     EXPECT_EQ(eval(x / y - y * y), -1.5);
 }
 
 TEST(Expr, symbol)
 {
-    auto x = value(5);
+    auto x = constant(5);
     auto y = symbol("y");
     auto expr = x / y - y * y;
-    std::get<impl::Symbol>(*y).value = value(2);
+    std::get<impl::Symbol>(*y).value = constant(2);
     EXPECT_EQ(eval(expr), -1.5);
-    std::get<impl::Symbol>(*y).value = value(2.5);
+    std::get<impl::Symbol>(*y).value = constant(2.5);
     EXPECT_EQ(eval(expr), -4.25);
     std::get<impl::Symbol>(*y).value = x;
     EXPECT_EQ(eval(expr), -24);
