38795f2368fdd8664e576f45e6358e92370c8cf7
Bowen Fu <missing>
2021-07-01 22:21:47 +0800

    Fix rational add constant.

--- test/mathia/auto-simplification.cpp
+++ test/mathia/auto-simplification.cpp
@@ -1,5 +1,6 @@
 #include "mathia.h"
 #include "gtest/gtest.h"
+#include <iostream>
 using namespace mathia;
 
 TEST(Simplification, power)
@@ -126,4 +127,90 @@ TEST(asBaseAndExp, 2)
         pattern | _ = expr(false)
     );
     EXPECT_TRUE(result);
+}
+
+TEST(Simplification, mul2Pow)
+{
+    auto const x = symbol("x");
+    EXPECT_EQ(toString(x * x), "(^ x 2)");
+    EXPECT_EQ(toString(x * x * x), "(^ x 3)");
+}
+
+TEST(Simplification, distributive2_1)
+{
+    auto const n2 = constant(2);
+    auto const x = symbol("x");
+    auto const n3 = constant(3);
+    auto const e = n2 * x + n3 * x;
+    EXPECT_EQ(toString(e), "(* 5 x)");
+}
+
+TEST(Simplification, distributive2_2)
+{
+    auto const n2 = constant(2);
+    auto const x = symbol("x");
+    auto const f3o2 = fraction(3, 2);
+    auto const e = n2 * x + f3o2 * x;
+    EXPECT_EQ(toString(e), "(* 7/2 x)");
+}
+
+TEST(simplifyRational, int)
+{
+    auto const n2 = constant(2);
+    auto const n3 = constant(3);
+
+    EXPECT_EQ(toString(n2 + n3), "5");
+}
+
+TEST(simplifyRational, rational_)
+{
+    auto const f7o2 = fraction(14, 4);
+
+    EXPECT_EQ(toString(mathia::impl::simplifyRational(f7o2)), "7/2");
+}
+
+TEST(simplifyRational, rational)
+{
+    auto const n2 = constant(2);
+    auto const f3o2 = fraction(3, 2);
+
+    EXPECT_EQ(toString(n2 + f3o2), "7/2");
+}
+
+TEST(simplifyRational, rational2)
+{
+    auto const f3o2 = fraction(3, 2);
+
+    EXPECT_EQ(toString(f3o2 + f3o2), "(* 3/2 2)");
+}
+
+
+TEST(asCoeffAndRest, rational)
+{
+    auto const n2 = constant(2);
+    auto const x = symbol("x");
+    auto const f3o2 = fraction(3, 2);
+
+    using namespace matchit;
+    Id<std::shared_ptr<mathia::impl::Expr>> coeff1, coeff2, rest;
+    auto result = match(*(n2 * x), *(f3o2 * x))
+    (
+        pattern | ds(mathia::impl::asCoeffAndRest(coeff1, rest), mathia::impl::asCoeffAndRest(coeff2, rest))= [&]
+        {
+            ((*coeff1) + (*coeff2)); // * (*rest);
+            return (*coeff1) == n2 && (*coeff2) == f3o2 && (*rest) == symbol("x"); },
+        pattern | _ = expr(false)
+    );
+    EXPECT_TRUE(result);
+}
+
+
+TEST(Simplification, distributive2)
+{
+    auto const n2 = constant(2);
+    auto const x = symbol("x");
+    auto const y = symbol("y");
+    auto const f3o2 = fraction(3, 2);
+    auto const e = n2 * x + y + f3o2 * x;
+    EXPECT_EQ(toString(e), "(+ (* 3/2 x) (* 2 x) y)");
 }
