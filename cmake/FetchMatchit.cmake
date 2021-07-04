29223cc61b081cc262b6251b2fca87161ead6854
Bowen Fu <missing>
2021-07-04 22:37:18 +0800

    Fix build.

--- cmake/FetchMatchit.cmake
+++ cmake/FetchMatchit.cmake
@@ -5,11 +5,6 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/BowenFu/matchit.cpp.git
     GIT_TAG main)
 
-FetchContent_GetProperties(matchit)
-if(NOT matchit_POPULATED)
-    FetchContent_Populate(matchit)
-    add_subdirectory(${matchit_SOURCE_DIR} ${matchit_BINARY_DIR}
-                    EXCLUDE_FROM_ALL)
-endif()
+FetchContent_MakeAvailable(matchit)
 
 message(STATUS "Matchit header are present at ${matchit_SOURCE_DIR}")
