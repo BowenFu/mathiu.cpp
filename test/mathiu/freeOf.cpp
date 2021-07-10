1ddef532cbcf1ee33b22ecbcf09ea0453095c02f
Bowen Fu <missing>
2021-07-10 16:50:51 +0800

    Free-of test.

--- test/mathiu/freeOf.cpp
+++ test/mathiu/freeOf.cpp
@@ -12,3 +12,13 @@ TEST(freeOf, 2)
 {
     EXPECT_TRUE(freeOf("a"_s + "b"_s, "c"_s));
 }
+
+TEST(freeOf, 3)
+{
+    EXPECT_FALSE(freeOf(("a"_s + "b"_s) * "c"_s, "a"_s + "b"_s));
+}
+
+TEST(freeOf, 4)
+{
+    EXPECT_FALSE(freeOf(sin("x"_s) + 2_i * "x"_s, sin("x"_s)));
+}
