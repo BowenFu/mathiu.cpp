e655c5e7913e2d32e01d471fadafe8f9e0cd8677
Bowen Fu <missing>
2021-07-11 21:50:02 +0800

    Solve with domain.

--- include/mathiu/function_range.h
+++ include/mathiu/function_range.h
@@ -3,6 +3,7 @@
 
 #include "matchit.h"
 #include "core.h"
+#include "solve.h"
 
 namespace mathiu
 {
@@ -10,13 +11,24 @@ namespace mathiu
     {
         // implement limit
 
-        inline CCInterval functionRange(ExprPtr const& function, ExprPtr const& symbol, CCInterval const& domain)
+        inline CCInterval functionRange(ExprPtr const& function, ExprPtr const& symbol, ExprPtr const& domain)
         {
+            auto const domain_ = std::get<CCInterval>(*domain);
             auto const subs = [&] (ExprPtr const& dst) { return substitute(function, symbol >> dst); };
-            auto const firstP = subs(domain.first);
-            auto const secondP = subs(domain.second);
+            auto const firstP = subs(domain_.first);
+            auto const secondP = subs(domain_.second);
             // TODO solve diff = 0 within the domain.
-            return CCInterval{{min(firstP, secondP), max(firstP, secondP)}};
+            auto const derivative = diff(function, symbol);
+            auto const criticalPoints = solve(derivative, symbol, domain);
+            auto min_ = min(firstP, secondP);
+            auto max_ = max(firstP, secondP);
+            for (auto const& e : std::get<Set>(*criticalPoints))
+            {
+                auto const v = subs(e);
+                min_ = min(min_, v);
+                max_ = max(max_, v);
+            }
+            return CCInterval{{min_, max_}};
         }
     } // namespace impl
 } // namespace mathiu
--- test/mathiu/functionRange.cpp
+++ test/mathiu/functionRange.cpp
@@ -5,14 +5,14 @@ using namespace mathiu;
 
 TEST(functionRange, 1)
 {
-    auto const range = functionRange("x"_s + 1_i, "x"_s, impl::CCInterval{{1_i, 2_i}});
+    auto const range = functionRange("x"_s + 1_i, "x"_s, std::make_shared<impl::Expr const>(impl::CCInterval{{1_i, 2_i}}));
     EXPECT_EQ(toString(range.first), "2");
     EXPECT_EQ(toString(range.second), "3");
 }
 
 TEST(functionRange, 2)
 {
-    auto const range = functionRange(-2_i * "x"_s + 1_i, "x"_s, impl::CCInterval{{1_i, 2_i}});
+    auto const range = functionRange(-2_i * "x"_s + 1_i, "x"_s, std::make_shared<impl::Expr const>(impl::CCInterval{{1_i, 2_i}}));
     EXPECT_EQ(toString(range.first), "-3");
     EXPECT_EQ(toString(range.second), "-1");
 }
